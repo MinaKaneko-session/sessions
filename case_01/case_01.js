0c44b10eb84e9abe8334ba8cedd03d8bff13976c
minakaneko <mina.kaneko@kiramex.com>
2022-01-22 16:24:53 +0900

    01

---
+++ case_01/case_01.js
@@ -0,0 +1,27 @@
+// var test = '';
+
+// $(window).on('load', function () {
+//   alert('すべてのコンテンツ読み込み時に実行');
+//   test = 'testです';
+//   alert(test);
+// });
+
+// alert('1js読み込み時に実行');
+
+// $(document).ready(function() { 
+//   // 〜処理〜 
+//   alert('2JQueryのライブラリ読み込み時に実行');
+// });
+
+// $(function () {
+//   alert('1JQueryのライブラリ読み込み時に実行');
+//   alert(test);
+// });
+
+// alert('2js読み込み時に実行');
+
+var test2 = 'テスト２です';
+console.log(test2);
+console.log(window);
+console.log(document);
+console.log($);
